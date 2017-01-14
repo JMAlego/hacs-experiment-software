881478b615fc7c989c152691ff4540081bb81df1
Jacob Allen <jacob@davidallenconsultancy.co.uk>
2017-01-14 18:14:37 +0000

    Compatibility Fixes and Crypto Nonce Addition
    Fixed some const scope issues and fixed the fact that the data input
    could be DOSed with the addtion of a crypto nonce.

--- server.js
+++ server.js
@@ -1,34 +1,86 @@
 const fs = require('fs');
 const url = require('url');
+const crypto = require('crypto');
+//Defining these as const in if statements is better, but doesn't work on some platforms so vars instead
+//This is due to the fact that consts defined in if statements are limited to the if statement's scope
+//While this is weird it is the standard and newer versions of nodejs follow this rule
+//For backwards and forwards compatibility it's best to use vars instead as below
+//While this is annoying it's more efficient and compatible than the alternatives
+//For more information see: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/const
+var https;
+var http;
+var auth;
+var certificates;
+
+function btoa(str) {
+  var buffer;
+
+  if (str instanceof Buffer) {
+    buffer = str;
+  } else {
+    buffer = new Buffer(str.toString(), 'binary');
+  }
+
+  return buffer.toString('base64');
+}
 
 // Set the following to false to disable basic HTTP
 // authentication and ssl
 const secure = true;
 var PORT;
 if (secure){
-  const https = require('https');
-  const auth = require('basic-auth');
-  const certificates = require('./certificate.js').config;
+  https = require('https');
+  auth = require('basic-auth');
+  certificates = require('./certificate.js').config;
   PORT=443;
 }
 else{
-  const http = require('http');
+  http = require('http');
   PORT=8080;
 }
 
+
 function passHash(password){
-  const crypto = require('crypto');
-  const hash = crypto.createHmac('sha256', "some_random_salt_value")
+  return crypto.createHmac('sha256', "some_random_salt_value")
                    .update(password)
                    .digest('hex');
-  return hash;
+}
+
+//For more security and less spamability
+var cryptoNonceKeys = {};
+
+function generateCryptoNonceKey(){
+  let key = crypto.createHmac('sha256', "For reference: https://en.wikipedia.org/wiki/Cryptographic_nonce")
+                   .update(crypto.randomBytes(32))
+                   .digest('hex');
+  cryptoNonceKeys[key] = Date.now();
+  return key;
+}
+function checkCryptoNonceKey(key){
+  if(cryptoNonceKeys[generateCryptoNonceKey()] !== undefined){
+    if(Date.now() - cryptoNonceKeys[generateCryptoNonceKey()] < 3600000){
+      return true;
+    }else{
+      removeCryptoNonceKey(key);
+    }
+  }
+  return false;
+}
+function removeCryptoNonceKey(key){
+  if(cryptoNonceKeys[generateCryptoNonceKey()] !== undefined){
+    delete cryptoNonceKeys[generateCryptoNonceKey()];
+  }
 }
 
 // TODO If this is deployed to a publicly accessible server
 // then evaluate this once so we don't need to store the
 // password in the source code.
-const password = passHash("foo");
-const username = 'HACS';
+var password;
+var username;
+if (secure){
+  password = passHash("foo");
+  username = 'HACS';
+}
 
 function handleRequest(request, response){
   if (secure){
@@ -42,15 +94,17 @@ function handleRequest(request, response){
   }
   response.setHeader('Content-Type', 'text/html');
   if(request.url == "/"){
-    response.end(fs.readFileSync("index.html", 'utf8'));
+    response.end(fs.readFileSync("index.html", 'utf8').replace("%CRYPTONONCEKEY%", btoa(generateCryptoNonceKey())));
   }
   else if(request.url == "/jquery.js"){
+    response.setHeader('Content-Type', 'application/javascript');
     response.end(fs.readFileSync("jquery.js", 'utf8'));
   }
   else if(request.url.substr(0,11) == "/submit.php"){//JSON.stringify(
     var urlParts = url.parse(request.url, true);
     var query = urlParts.query;
-    if(query.code !== undefined && query.data !== undefined && query.code == "d9923118dd0861486b3569ba080486a4"){
+    if(query.code !== undefined && query.data !== undefined && checkCryptoNonceKey(query.code)){
+      removeCryptoNonceKey(query.code);
       var currentFile = fs.readFileSync("data/data.json", 'utf8');
       var jsonData = JSON.parse(currentFile);
       jsonData.push(JSON.parse(query.data));
