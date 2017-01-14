98b8b7d10129a3d7f51f816b1216492be34db0e4
Daniel Bailey <danieljabailey@gmail.com>
2017-01-14 15:54:16 +0000

    Added SSL support and basic http authentication

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
-const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const PORT=8080; 
+// Set the following to false to disable basic HTTP
+// authentication and ssl
+const secure = true;
+var PORT;
+if (secure){
+  const https = require('https');
+  const auth = require('basic-auth');
+  const certificates = require('./certificate.js').config;
+  PORT=443;
+}
+else{
+  const http = require('http');
+  PORT=8080;
+}
+
+function passHash(password){
+  const crypto = require('crypto');
+  const hash = crypto.createHmac('sha256', "some_random_salt_value")
+                   .update(password)
+                   .digest('hex');
+  return hash;
+}
+
+// TODO If this is deployed to a publicly accessible server
+// then evaluate this once so we don't need to store the
+// password in the source code.
+const password = passHash("foo");
+const username = 'HACS';
 
 function handleRequest(request, response){
+  if (secure){
+    var credentials = auth(request);
+    if (!credentials || credentials.name !== username || passHash(credentials.pass) !== password) {
+      response.statusCode = 401;
+      response.setHeader('WWW-Authenticate', 'Basic realm="Experiment"');
+      response.end('Access denied');
+      return;
+    }
+  }
   response.setHeader('Content-Type', 'text/html');
   if(request.url == "/"){
     response.end(fs.readFileSync("index.html", 'utf8'));
@@ -29,8 +64,19 @@ function handleRequest(request, response){
   }
 }
 
-var server = http.createServer(handleRequest);
+var protocol;
+if (secure){
+  var server = https.createServer(certificates, handleRequest);
+  protocol = 'https';
+}
+else{
+  var server = http.createServer(handleRequest);
+  protocol = 'http';
+}
 
 server.listen(PORT, function(){
-  console.log("Server listening on: http://localhost:%s", PORT);
+  console.log("Server listening on: %s://localhost:%s", protocol, PORT);
+  if (secure){
+    console.log("Password hash: %s", password);
+  }
 });
